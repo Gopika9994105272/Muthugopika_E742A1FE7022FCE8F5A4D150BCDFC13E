c56ceaac3bd550e1ab6c5d4f56ebbbbdcd1fd0c7
Gopika9994105272 <144932799+Gopika9994105272@users.noreply.github.com>
2023-09-20 20:27:14 +0530

    Add files via upload

--- main (4).py
+++ main (4).py
@@ -1,19 +1,30 @@
-#leap year
-"""
-  year%4 == 0 &
-  year%100 != 0 / 
-  year % 400 == 0
-"""
- 
+"""Imlement a class called player that represents a cricket player. The player class should have a 
+method called play() which prints "The player is playing cricket. Derive two classes,Batsman and
+Bowler, from th player class. Override the play() method in each derived class to print "The batsman 
+is batting" and "The bowler is bowling",respectively. Write a program to create objects of both the
+Batsman and Bowler classes and call the play() method for each object"""
 
-def isLeapYear(year):
-  if(year%4 == 0 and year % 100 !=0) or year % 400 == 0:
-    return True 
-  else:
-    return False
-year=int(input("Enter the year:"))
-if isLeapYear(year):
-  print("{}is a leap year".format(year))
-else:
-  print("{} is not a leap year".format(year))
-  
+
+#define the base class Player
+class Player:
+  def Play(self):
+    print("The player is playing cricket.")
+
+
+#define the derived class Batsman
+class Batsman(Player):
+  def play(self):
+    print("The batsman is batting")
+
+#Define the derived class Bowler
+class Bowler(Player):
+  def play(self):
+    print("The bowler is bowling")
+
+#create objects of batsman and bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+#call the play() method for each object
+batsman.play()
+bowler.play()
